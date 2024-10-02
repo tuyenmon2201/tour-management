d9f949f2bd87c2aae73befcc27a81acf57b9a44e
tuyenmon2201 <letuyen22012202@gmail.com>
2024-10-02 22:31:49 +0700

    Create route basic and install PUG

--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 3003;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Trang chủ");
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/tours", (req: Request, res: Response) => {
+    res.render("client/pages/tours/index");
 });
 
 app.listen(port, () => {
