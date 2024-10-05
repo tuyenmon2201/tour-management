5f7435c468bde3541017682be79004f24f8b55a4
tuyenmon2201 <letuyen22012202@gmail.com>
2024-10-05 16:54:29 +0700

    Get tour list

--- index.ts
+++ index.ts
@@ -6,14 +6,29 @@ dotenv.config();
 import sequelize from "./config/database";
 sequelize
 
+import Tour from "./models/tour.model";
+
 const app: Express = express();
 const port: number | string = process.env.PORT || 3003;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/tours", (req: Request, res: Response) => {
-    res.render("client/pages/tours/index");
+app.get("/tours", async (req: Request, res: Response) => {
+    // SELECT * FROM tours WHERE deleted = false AND status = "active";
+
+    const tours = await Tour.findAll({
+        where: {
+            deleted: false,
+            status: "active"
+        },
+        raw: true
+    });
+
+    res.render("client/pages/tours/index", {
+        pageTitle: "Trang danh sách tour",
+        tours: tours
+    });
 });
 
 app.listen(port, () => {
