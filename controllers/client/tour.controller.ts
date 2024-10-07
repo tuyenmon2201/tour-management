7a970ec891c2905d238f1a10d204d9b642190f99
tuyenmon2201 <letuyen22012202@gmail.com>
2024-10-08 00:37:01 +0700

    Create interface frames

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -39,3 +39,14 @@ export const index = async (req: Request, res: Response) => {
         tours: tours
     });
 }
+
+export const detail = async (req: Request, res: Response) => {
+
+    const slugTour = req.params.slugTour;
+
+    // console.log(slugTour);
+    
+    res.render("client/pages/tours/detail", {
+        pageTitle: "Chi tiết tour"
+    });
+}
--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -5,4 +5,6 @@ import * as controller from "../../controllers/client/tour.controller";
 
 router.get("/:slugCategory", controller.index);
 
+router.get("/detail/:slugTour", controller.detail);
+
 export const tourRoute = router;
