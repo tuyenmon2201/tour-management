5c8bec021bb9d08f2a1ca3797471d8920c2567e5
tuyenmon2201 <letuyen22012202@gmail.com>
2024-10-08 00:25:09 +0700

    Created tour list page by category (on the client side)

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,16 +1,38 @@
 import Tour from "../../models/tour.model";
 import { Request, Response } from "express";
+import { QueryTypes } from 'sequelize';
+import sequelize from "../../config/database";
 
 export const index = async (req: Request, res: Response) => {
-    // SELECT * FROM tours WHERE deleted = false AND status = "active";
+    const slugCategory = req.params.slugCategory;
 
-    const tours = await Tour.findAll({
-        where: {
-            deleted: false,
-            status: "active"
-        },
-        raw: true
+    // console.log(slugCategory);
+
+    const tours = await sequelize.query(`
+        SELECT tours.*, ROUND(price * (1 - discount/100)) AS price_special
+        FROM tours
+        JOIN tours_categories ON tours.id = tours_categories.tour_id
+        JOIN categories ON tours_categories.category_id = categories.id
+        WHERE
+            categories.slug = '${slugCategory}'
+            AND categories.deleted = false
+            AND categories.status = 'active'
+            AND tours.deleted = false
+            AND tours.status = 'active';
+    `, {
+        type: QueryTypes.SELECT,
     });
+
+    for (const item of tours) {
+        if(item["images"]){
+            const arrayImage = JSON.parse(item["images"]);
+            if(arrayImage.length > 0){
+                item["image"] = arrayImage[0];
+            }
+        }
+
+        item["price_special"] = parseInt(item["price_special"]);
+    }
 
     res.render("client/pages/tours/index", {
         pageTitle: "Trang danh sách tour",
--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -3,6 +3,6 @@ const router = express.Router();
 
 import * as controller from "../../controllers/client/tour.controller";
 
-router.get("/", controller.index);
+router.get("/:slugCategory", controller.index);
 
 export const tourRoute = router;
