3419e5061bd04522cfa2b0462117b021772f18b7
tuyenmon2201 <letuyen22012202@gmail.com>
2024-10-08 22:15:39 +0700

    Add tour to cart

--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,38 @@ if(tourImages){
     });
 }
 // End tour-images
+
+// Gio hang
+const cart = localStorage.getItem("cart");
+if(!cart){
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if(formAddToCart){
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+    const quantity = parseInt(formAddToCart.quantity.value);
+
+    if(tourId && quantity.length > 0){
+      const cart = JSON.parse(localStorage.getItem("cart"));
+
+      const existTour = cart.find(item => item.tourId == tourId);
+
+      if(existTour){
+        existTour.quantity += quantity;
+      } else{
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        });
+      }
+
+      localStorage.setItem("cart", JSON.parse(cart));
+    }
+
+  })
+}
+// Het gio hang
