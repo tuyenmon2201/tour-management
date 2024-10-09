02df890bcf005d620d90247082c8b0e6dee23939
tuyenmon2201 <letuyen22012202@gmail.com>
2024-10-09 19:54:02 +0700

    Show notification added tour to cart

--- public/js/script.js
+++ public/js/script.js
@@ -10,6 +10,18 @@ if(tourImages){
 }
 // End tour-images
 
+// alert-add-cart-susscess
+const alertAddCartSusscess = () => {
+  const elementAlert = document.querySelector("[alert-add-cart-susscess]");
+  if(elementAlert) {
+    elementAlert.classList.remove("alert-hidden");
+    setTimeout(() => {
+      elementAlert.classList.add("alert-hidden");
+    }, 3000);
+  }
+}
+// End alert-add-cart-susscess
+
 // Gio hang
 const cart = localStorage.getItem("cart");
 if(!cart){
@@ -39,6 +51,8 @@ if(formAddToCart){
       }
 
       localStorage.setItem("cart", JSON.stringify(cart));
+      
+      alertAddCartSusscess();
     }
 
   })
