99b22927797d98ae41584986af52bf774adb7d75
tuyenmon2201 <letuyen22012202@gmail.com>
2024-10-12 14:12:58 +0700

    Server get data to save in orders model

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,9 +1,35 @@
 import { Request, Response } from "express";
+import Order from "../../models/order.model";
+import { generateOrderCode } from "../../helpers/generate.helper";
 
 // [GET] /cart/
 export const index = async (req: Request, res: Response) => {
+    const data = req.body;
+
+    // Lưu data vào bảng orders
+    const dataOrders = {
+        code: "",
+        fullName: data.info.fullName,
+        phone: data.info.phone,
+        note: data.info.note,
+        status: "initial",
+    };
+
+    const order = await Order.create(dataOrders);
+    const orderId = order.dataValues.id;
+    const code = generateOrderCode(orderId);
+
+    await Order.update({
+        code: code
+    }, {
+        where: {
+            id: orderId
+        }
+    });
+
     res.json({
         code: 200,
-        message: "Đặt hàng thành công"
+        message: "Đặt hàng thành công",
+        orderCode: code
     })
 };
