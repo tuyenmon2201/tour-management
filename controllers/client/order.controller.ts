0d4bdd08a79dac322c1f4a956b86ee43cb8be20b
tuyenmon2201 <letuyen22012202@gmail.com>
2024-10-12 14:22:28 +0700

    Server get data to save in orders-item model

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import Order from "../../models/order.model";
 import { generateOrderCode } from "../../helpers/generate.helper";
+import Tour from "../../models/tour.model";
+import OrderItem from "../../models/order-item.model";
 
 // [GET] /cart/
 export const index = async (req: Request, res: Response) => {
@@ -26,6 +28,32 @@ export const index = async (req: Request, res: Response) => {
             id: orderId
         }
     });
+    // Het Lưu data vào bảng orders
+
+    // Lưu data vào bảng orders_item
+    for (const item of data.cart) {
+        const dataItem = {
+            orderId: orderId,
+            tourId: item.tourId,
+            quantity: item.quantity,
+        };
+
+        const tourInfo = await Tour.findOne({
+            where: {
+                id: item.tourId,
+                deleted: false,
+                status: 'active',
+            },
+            raw: true,
+        });
+
+        dataItem["price"] = tourInfo["price"];
+        dataItem["discount"] = tourInfo["discount"];
+        dataItem["timeStart"] = tourInfo["timeStart"];
+
+        await OrderItem.create(dataItem);
+    }
+    // Hết Lưu data vào bảng orders_item
 
     res.json({
         code: 200,
